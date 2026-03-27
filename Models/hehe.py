bdc9072271129599fc0404e5f197555433b4bb7b
Taksh Diyora <takshdiyora40@gmail.com>
2026-03-27 14:02:38 +0530

    Added Linear Regression from scratch

---
+++ Models/hehe.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class LinearRegression():
+    def __init__(self, n_iterations = 10000, learning_rate = 0.01):
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+        self.w = None
+        self.b = None
+    
+    def fit(self, X, y):
+        n_samples = X.shape[0]
+        n_features = X.shape[1]
+        
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        for _ in range(self.n_iterations):
+            f_wb = np.dot(X, self.w) + self.b
+            error = f_wb - y
+
+            dw = np.dot(X.T, error) / n_samples
+            db = np.sum(error) / n_samples
+
+            self.w = self.w - self.learning_rate * dw
+            self.b = self.b - self.learning_rate * db
+
+    def predict(self, X):
+        return np.dot(X, self.w) + self.b
+    
+    def score(self, X, y):
+        f_wb = self.predict(X)
+        ss_res = np.sum((y-f_wb)**2)
+        ss_tot = np.sum((y - np.mean(y))**2)
+
+        return 1 - ss_res/ss_tot
